b93d4dc4a490a848c74686da4dd10c9b841d6252
kaviyarsan2004 <kavi580580@gmail.com>
2025-09-25 14:50:03 -0400

    check

--- cec-portal/vite.config.js
+++ cec-portal/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Kaviyarsan2004.github.io/CEC_portal_deploy/cec-portal',
+  base: '/CEC_portal_deploy/cec-portal',
 })
