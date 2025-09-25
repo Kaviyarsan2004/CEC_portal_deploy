2a31717e8819376877324a8813f4a167e1e89382
kaviyarsan2004 <kavi580580@gmail.com>
2025-09-25 14:36:34 -0400

    deploy scripts alter

--- cec-portal/vite.config.js
+++ cec-portal/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Kaviyarsan2004.github.io/CEC_portal_deploy/',
+  base: 'https://Kaviyarsan2004.github.io/CEC_portal_deploy/cec-portal',
 })
