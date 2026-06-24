20dfc151d678790ed8f9f9d45f27511f956b1a17
Kaviyarsan2004 <kavi580580@gmail.com>
2026-06-24 14:33:47 -0400

    updated mode selection

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/CEC_portal_deploy/',
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+  },
 })
